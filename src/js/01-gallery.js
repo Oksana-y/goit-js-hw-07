a0992bb77944a97b8d817861cbfa0aea6e755ccd
Oksana Yerep <114570807+Oksana-y@users.noreply.github.com>
2022-12-23 16:28:57 +0200

    Working moments

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,32 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const gallery = document.querySelector(".gallery");
+
+
+function createGalleryItems(){
+   galleryItems
+    .map(({preview, original, description}) => {
+     `<div class="gallery__item">
+        <a class="gallery__link" href="large-image.jpg">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </div>`;
+    })
+    .join("");
+}
+
 
 console.log(galleryItems);
+
+
+gallery.insertAdjacentHTML("afterbegin", createGalleryItems()); 
+
+
+
+// console.log(galleryItems);
+
